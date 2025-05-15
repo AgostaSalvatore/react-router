14f4018765249fb4329911d8f1d0bcb2af22e599
AgostaSalvatore <agosta.boolean@gmail.com>
2025-05-15 13:38:43 +0200

    feat: initialize React app with router and axios dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+//import axios
 import axios from 'axios'
+//impost useState useEffect
 import { useState, useEffect } from 'react'
+//import Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css"
+//import Routers
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
