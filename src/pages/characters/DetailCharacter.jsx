9e44f14201ad7333651ba58d143f52317da786fa
AgostaSalvatore <agosta.boolean@gmail.com>
2025-05-16 13:20:38 +0200

    feat: create DetailCharacter component with basic structure and routing imports

--- src/pages/characters/DetailCharacter.jsx
+++ src/pages/characters/DetailCharacter.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import axios from 'axios'
+import { useParams, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const DetailCharacter = () => {
+    const params = useParams();
+    const id = parseInt(params.id);
+    const navigate = useNavigate();
+
+    const [character, setCharacter] = useState({})
+
     return (
         <div>DetailCharacter</div>
     )
